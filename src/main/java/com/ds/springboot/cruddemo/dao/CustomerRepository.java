5b45839da590c2638b36780edc3fef334ee13e98
divyasourabh <divyasourabh@gmail.com>
2020-05-03 13:48:52 +0530

    Configure Spring DATA REST Project

--- src/main/java/com/ds/springboot/cruddemo/dao/CustomerRepository.java
+++ src/main/java/com/ds/springboot/cruddemo/dao/CustomerRepository.java
@@ -1,9 +1,11 @@
 package com.ds.springboot.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ds.springboot.cruddemo.entity.Customer;
 
+@RepositoryRestResource(path="customers") //By default JpaRepository set small case entity name with suffix 's' as endpoint, we can change it by this
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
 	//That's it... no need to write any code //
